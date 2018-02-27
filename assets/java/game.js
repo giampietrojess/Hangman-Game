2dc484fe3340c2a766ea871485e1426409056911
Jess Giampietro <JessGiampietro@Jesss-MacBook-Pro.local>
2018-02-27 12:55:31 -0500

    add basic html and declare starting variables

---
+++ assets/java/game.js
@@ -0,0 +1,12 @@
+
+
+// Options for Random Words
+
+    var randomWords = ["Hawkeye","Iron Man","Spider Man","The Incredible Hulk","Black Widow","Captain America","Thor","Wolverine","Daredevil","Dr. Strange","Black Panther","Deadpool"]
+        wins = 0
+        losses = 0
+        guessesLeft = 10
+        guessedLetters = " "
+        wrongLetters = " "
+
+        
