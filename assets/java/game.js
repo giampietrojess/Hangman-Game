d1c685b850d79037f672cf1cf74a36682d73b2e6
Jess Giampietro <JessGiampietro@Jesss-MacBook-Pro.local>
2018-03-01 15:46:41 -0500

    started working on new game function

--- assets/java/game.js
+++ assets/java/game.js
@@ -1,24 +1,54 @@
     //DOM elements to "hook" into
-    var newGameButton = document.getElementById('new-game-button');
-    var placeholders = document.getElementById('placeholders');
-    var guessedLetters = document.getElementById('guessed-letters');
-    var guessesLeft = document.getElementById('guesses-Left');
-    var wins = document.getElementById('wins');
-    var loses = document.getElementById('loses');
+    var newGamebutton = document.getElementById('new-game');
+        placeholders = document.getElementById('placeholders');
+        letterGuessed = document.getElementById('letter-guessed');
+        remainingGuesses = document.getElementById('remaining-guesses');
+        wins = document.getElementById('wins');
+        loses = document.getElementById('losses');
    
     // Options for Random Words
 
     var randomWords = ["Hawkeye","Iron Man","Spider Man","The Incredible Hulk","Black Widow","Captain America","Thor","Wolverine","Daredevil","Dr. Strange","Black Panther","Deadpool"]
-        
-    // Other Variables
-        wins = 0
-        losses = 0
-        guessesLeft = 10
-        guessedLetters = " "
-        wrongLetters = " "
+        wins = 0;
+        losses = 0;
+        guessesLeft = 10;
+        gameRunning = false;
+        mysteryWord = '';
+        mysteryWordPlaceholderArr = [];
+        guessedLettersBox = [];
+        wrongLettersBox = [];
 
 
     // Start the Game function
+
+    function newGame() {
+
+        gameRunning = true;
+        guessesLeft = 10;
+        guessedLettersBox = [];
+        wrongLettersBox = [];
+        mysteryWordPlaceholderArr = [];
+
+        //This chooses the new word for each new game
+        mysteryWord = randomWords[Math.floor(Math.random() * randomWords.length)];
+        //This randomly loops through the words in the Random Words Array
+        for (var i = 0; i < mysteryWord.length; i++) {
+            
+            // If there is a space in the mystery word, this pushes a blank space
+            if (mysteryWord[i] === ' ') {
+                mysteryWordPlaceholderArr.push(' ');
+            }
+            // Otherwise, each letter is replaced with an underscore
+            else {
+                mysteryWordPlaceholderArr.push('_');
+            }
+
+        }
+
+        remainingGuesses.textContent = guessesLeft;
+        placeholders.textContent = mysteryWordPlaceholderArr.join('');
+        letterGuessed.textContent = wrongLettersBox;
+
 
     // Add event listener
 
