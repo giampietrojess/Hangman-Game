be95247a197ad0a4ce51f439ecd74e76f10a7241
Jess Giampietro <JessGiampietro@Jesss-MacBook-Pro.local>
2018-03-01 15:26:28 -0500

    added more pseduocode and list elements

--- assets/java/game.js
+++ assets/java/game.js
@@ -1,12 +1,33 @@
-
-
-// Options for Random Words
+    //DOM elements to "hook" into
+    var newGameButton = document.getElementById('new-game-button');
+    var placeholders = document.getElementById('placeholders');
+    var guessedLetters = document.getElementById('guessed-letters');
+    var guessesLeft = document.getElementById('guesses-Left');
+    var wins = document.getElementById('wins');
+    var loses = document.getElementById('loses');
+   
+    // Options for Random Words
 
     var randomWords = ["Hawkeye","Iron Man","Spider Man","The Incredible Hulk","Black Widow","Captain America","Thor","Wolverine","Daredevil","Dr. Strange","Black Panther","Deadpool"]
+        
+    // Other Variables
         wins = 0
         losses = 0
         guessesLeft = 10
         guessedLetters = " "
         wrongLetters = " "
 
-        
+
+    // Start the Game function
+
+    // Add event listener
+
+    // Add onkeyup event for letter Guess
+
+    // Check if Guessed Letter is Correct and if so, push to Blank Space
+
+    // Check if Guessed Letter is Incorrect and if not, push to Incorrect Bank
+
+    // Check if Win
+
+    // Check if Lose
