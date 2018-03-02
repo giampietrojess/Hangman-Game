88fe5b03d380a3132e32d80ea3e5dce9a3c33b98
Jess Giampietro <JessGiampietro@Jesss-MacBook-Pro.local>
2018-03-01 20:17:06 -0500

    made the game basically work

--- assets/java/game.js
+++ assets/java/game.js
@@ -8,7 +8,7 @@
    
     // Options for Random Words
 
-    var randomWords = ["Hawkeye","Iron Man","Spider Man","The Incredible Hulk","Black Widow","Captain America","Thor","Wolverine","Daredevil","Dr. Strange","Black Panther","Deadpool"],
+    var randomWords = ["Hawkeye","Iron Man","Spider Man","The Incredible Hulk","Black Widow","Captain America","Thor","Wolverine","Daredevil","Doctor Strange","Black Panther","Deadpool"],
         wins = 0,
         losses = 0,
         guessesLeft = 10,
@@ -41,24 +41,67 @@
             }
             // Otherwise, each letter is replaced with an underscore
             else {
-                mysteryWordPlaceholderArr.push(' _ ');
+                mysteryWordPlaceholderArr.push('_');
             }
         }
-
+    
         DOMremainingGuesses.textContent = guessesLeft;
         DOMplaceholders.textContent = mysteryWordPlaceholderArr.join('');
-        DOMguessedLetters.textContent = wrongLettersBox;
+        DOMguessedLetters.textContent = wrongLettersBank;
 
     }
+    
     // Add event listener
-        DOMnewGamebutton.addEventListener('click', newGame);
+    DOMnewGamebutton.addEventListener('click', newGame);
         
     // Add onkeyup event for letter Guess
 
-    // Check if Guessed Letter is Correct and if so, push to Blank Space
+    document.onkeyup = function(event) {
+        console.dir(event);
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+            letterGuess(event.key);
+        }
+    }
 
+    function letterGuess(letter) {
+        console.log(letter);
+        if (gameRunning === true && guessedLettersBank.indexOf(letter) === -1) {
+            //Actual Game functions
+            guessedLettersBank.push(letter);
+            //Is Guessed Letter in Mystery Word?
+            for (var i = 0; i < mysteryWord.length; i++) {
+                //converts letters to lower case for comparison
+                if (mysteryWord[i].toLowerCase() === letter.toLowerCase()) {
+
+                    mysteryWordPlaceholderArr[i] = mysteryWord[i];
+                }        
+            }
+            DOMplaceholders.textContent = mysteryWordPlaceholderArr.join('');
+            checkIncorrect(letter);
+        }    
+        else {
+            if (!gameRunning) {
+                alert("Click the New Game Button to Play");
+            } else {
+            alert("You've already guessed this letter, try another!");
+            }
+        } 
+    }
+        
     // Check if Guessed Letter is Incorrect and if not, push to Incorrect Bank
 
+    function checkIncorrect(letter) {
+        if (
+            mysteryWordPlaceholderArr.indexOf(letter.toLowerCase()) === -1 
+            &&
+            mysteryWordPlaceholderArr.indexOf(letter.toUpperCase()) === -1 )        
+        {
+            guessesLeft--;
+            wrongLettersBank.push(letter);
+            DOMguessedLetters.textContent = wrongLettersBank.join(' ');
+            DOMremainingGuesses.textContent = guessesLeft;
+        }
+    }    
     // Check if Win
 
     // Check if Lost
